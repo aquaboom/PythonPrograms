d050b038642a2bbf375fc556d1796560fdee01b7
regesonali <rege.sonali@gmail.com>
2018-06-20 17:47:17 -0500

    first commit

---
+++ SumOfNumbersDivisible.py
@@ -0,0 +1,8 @@
+# To calculate sum of all the numbers that are divisible by 2 or 5 from 1 to 99
+
+
+sum=0 #Initializing sum as 0 
+for i in range(1,100):
+    if i%2==0 or i%5==0
+        sum=sum+i
+print(sum) 
