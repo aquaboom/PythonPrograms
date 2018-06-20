dd61f38b5a713908ef03b945d7af6713f2e49c43
regesonali <rege.sonali@gmail.com>
2018-06-20 18:40:54 -0500

    Create Dict.py

---
+++ Dict.py
@@ -0,0 +1,14 @@
+# To create a Dictionary in Python and print the value of a particular key
+
+def Dict():
+    d={} #This defines the dictionary d
+    d["Sonali"]=25                              # Key Sonali has a value 25 defined
+    d["Tom"]=16                                 # Key Tom has a value 16 defined
+    d["Harry"]=24                               # Key Harry has a value 24 defined
+    print("The age of Harry is:" + d["Harry"])
+    
+    
+ Output:
+ The age of Harry is: 24
+    
+    
